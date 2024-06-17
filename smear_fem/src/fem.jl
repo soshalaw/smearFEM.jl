3c543a33f9a27896f434cd6550ba907a02723414
soshalaw <soshalaweerathunge@gmail.com>
2024-06-17 16:32:27 +0200

    added code for 2d higher order basis functions

--- smear_fem/src/fem.jl
+++ smear_fem/src/fem.jl
@@ -20,7 +20,7 @@ module fem
         return ξ, w
     end
 
-    function basis_function(ξ,η=nothing,ζ=nothing)
+    function basis_function(ξ,η=nothing,ζ=nothing,FunctionClass = "Q1")
         """ Define the basis functions and the gradients for a master element
             xi, eta, zeta are the coordinates of the point where the basis function is evaluated
             FunctionClass is the type of basis functions to be considered
@@ -29,47 +29,89 @@ module fem
             N: basis functions {[ndof] Vector{Float64}}
             Delta_N: gradient of the basis functions {[ndof,ndim] Matrix{Float64}}
         """
-        if !isnothing(ζ) # Considering a 3D master element
-            # basis functions
-            N = [(1-ξ)*(1-η)*(1-ζ)/8, 
-                (1+ξ)*(1-η)*(1-ζ)/8, 
-                (1+ξ)*(1+η)*(1-ζ)/8, 
-                (1-ξ)*(1+η)*(1-ζ)/8, 
-                (1-ξ)*(1-η)*(1+ζ)/8, 
-                (1+ξ)*(1-η)*(1+ζ)/8, 
-                (1+ξ)*(1+η)*(1+ζ)/8, 
-                (1-ξ)*(1+η)*(1+ζ)/8]
-
-            # gradient of the basis functions
-            ΔN = [[-(1-η)*(1-ζ)/8, (1-η)*(1-ζ)/8, (1+η)*(1-ζ)/8, -(1+η)*(1-ζ)/8, -(1-η)*(1+ζ)/8, (1-η)*(1+ζ)/8, (1+η)*(1+ζ)/8, -(1+η)*(1+ζ)/8] [-(1-ξ)*(1-ζ)/8, -(1+ξ)*(1-ζ)/8, (1+ξ)*(1-ζ)/8, (1-ξ)*(1-ζ)/8, -(1-ξ)*(1+ζ)/8, -(1+ξ)*(1+ζ)/8, (1+ξ)*(1+ζ)/8, (1-ξ)*(1+ζ)/8] [-(1-ξ)*(1-η)/8, -(1+ξ)*(1-η)/8, -(1+ξ)*(1+η)/8, -(1-ξ)*(1+η)/8, (1-ξ)*(1-η)/8, (1+ξ)*(1-η)/8, (1+ξ)*(1+η)/8, (1-ξ)*(1+η)/8]]  # [dN/dxi dN/deta dN/dzeta]
-        elseif !isnothing(η) # Considering a 2D master element
-            # basis functions
-            N = [(1-ξ)*(1-η)/4, (ξ+1)*(1-η)/4, (1+ξ)*(η+1)/4, (1-ξ)*(1+η)/4]
-
-            # gradient of the basis functions
-            ΔN = [[-(1-η)/4 , (1-η)/4, (η+1)/4, -(1+η)/4] [-(1-ξ)/4, -(ξ+1)/4, (1+ξ)/4, (1-ξ)/4] ]
-        else # Considering a 1D master element
-            # basis functions
-            N  = [0.5-0.5*ξ, 0.5+0.5*ξ]
-
-            # gradient of the basis functions
-            ΔN = [-0.5 0.5]
+        
+        if FunctionClass == "Q2"
+            if !isnothing(η) # Considering a 2D master element
+                # basis functions
+                N = [(1-ξ)*ξ*(1-η)*η/4, 
+                    -ξ*(1+ξ)*(1-η)*η/4, 
+                    ξ*(1+ξ)*η*(1+η)/4, 
+                    -(1-ξ)*ξ*η*(1+η)/4,
+                    -(1-ξ)*(1+ξ)*(1-η)*η/2,
+                    ξ*(1+ξ)*(1-η)*(1+η)/2,
+                    (1-ξ)*(1+ξ)*η*(1+η)/2,
+                    -(1-ξ)*ξ*(1-η)*(1+η)/2,
+                    (1-ξ)*(1+ξ)*(1-η)*(1+η)] # gradient of the basis functions
+    
+                Delta_Nx = [(1-2*ξ)*(1-η)*η/4, 
+                    -(1+2*ξ)*(1-η)*η/4, 
+                    (1+2*ξ)*η*(1+η)/4, 
+                    -(1-2*ξ)*η*(1+η)/4,
+                    ξ*(1-η)*η,
+                    (1+2*ξ)*(1-η)*(1+η)/2,
+                    -ξ*η*(1+η),
+                    -(1-2*ξ)*(1-η)*(1+η)/2,
+                    -2*ξ*(1-η)*(1+η)] # dN/dξ gradient of the basis functions
+                
+                Delta_Ny = [(1-ξ)*ξ*(1-2*η)/4, 
+                    -ξ*(1+ξ)*(1-2*η)/4, 
+                    ξ*(1+ξ)*(1+2*η)/4, 
+                    -(1-ξ)*ξ*(1+2*η)/4,
+                    -(1-ξ)*(1+ξ)*(1-2*η)/2,
+                    -ξ*(1+ξ)*η,
+                    (1-ξ)*(1+ξ)*(1+2*η)/2,
+                    (1-ξ)*ξ*η,
+                    -(1-ξ)*(1+ξ)*2*η] # dN/dη gradient of the basis functions
+            
+                ΔN = [Delta_Nx Delta_Ny] # [dN/dξ dN/dη]
+            end
+        elseif FunctionClass == "Q1"
+            if !isnothing(ζ) # Considering a 3D master element
+                # basis functions
+                N = [(1-ξ)*(1-η)*(1-ζ)/8, 
+                    (1+ξ)*(1-η)*(1-ζ)/8, 
+                    (1+ξ)*(1+η)*(1-ζ)/8, 
+                    (1-ξ)*(1+η)*(1-ζ)/8, 
+                    (1-ξ)*(1-η)*(1+ζ)/8, 
+                    (1+ξ)*(1-η)*(1+ζ)/8, 
+                    (1+ξ)*(1+η)*(1+ζ)/8, 
+                    (1-ξ)*(1+η)*(1+ζ)/8]
+    
+                # gradient of the basis functions
+                ΔN = [[-(1-η)*(1-ζ)/8, (1-η)*(1-ζ)/8, (1+η)*(1-ζ)/8, -(1+η)*(1-ζ)/8, -(1-η)*(1+ζ)/8, (1-η)*(1+ζ)/8, (1+η)*(1+ζ)/8, -(1+η)*(1+ζ)/8] [-(1-ξ)*(1-ζ)/8, -(1+ξ)*(1-ζ)/8, (1+ξ)*(1-ζ)/8, (1-ξ)*(1-ζ)/8, -(1-ξ)*(1+ζ)/8, -(1+ξ)*(1+ζ)/8, (1+ξ)*(1+ζ)/8, (1-ξ)*(1+ζ)/8] [-(1-ξ)*(1-η)/8, -(1+ξ)*(1-η)/8, -(1+ξ)*(1+η)/8, -(1-ξ)*(1+η)/8, (1-ξ)*(1-η)/8, (1+ξ)*(1-η)/8, (1+ξ)*(1+η)/8, (1-ξ)*(1+η)/8]]  # [dN/dxi dN/deta dN/dzeta]
+            elseif !isnothing(η) # Considering a 2D master element
+                # basis functions
+                N = [(1-ξ)*(1-η)/4, (ξ+1)*(1-η)/4, (1+ξ)*(η+1)/4, (1-ξ)*(1+η)/4]
+    
+                # gradient of the basis functions
+                ΔN = [[-(1-η)/4 , (1-η)/4, (η+1)/4, -(1+η)/4] [-(1-ξ)/4, -(ξ+1)/4, (1+ξ)/4, (1-ξ)/4] ]
+            else # Considering a 1D master element
+                # basis functions
+                N  = [0.5-0.5*ξ, 0.5+0.5*ξ]
+    
+                # gradient of the basis functions
+                ΔN = [-0.5 0.5]
+            end
         end
         return N, ΔN
     end
 
-    function assemble_system(ne, NodeList, IEN, ndim)
+    function assemble_system(ne, NodeList, IEN, ndim, FunctionClass)
         """ Assembles the finite element system. Returns the global stiffness matrix
-    
+
             Returns:
             K: sparse stiffness matrix {[ndof,ndof] SparseMatrixCSC{Float64,Int64}}
         """
         # (I,J,V) vectors for COO sparse matrix
-
-        E = zeros(Int64, (2^2*ne)^ndim)
-        J = zeros(Int64, (2^2*ne)^ndim)
-        V = zeros(Float64, (2^2*ne)^ndim)
-
+        if FunctionClass == "Q1"
+            E = zeros(Int64, (2^ndim*ne)^ndim)
+            J = zeros(Int64, (2^ndim*ne)^ndim)
+            V = zeros(Float64, (2^ndim*ne)^ndim)
+        elseif FunctionClass == "Q2"
+            E = zeros(Int64, (3^ndim*ne)^ndim)
+            J = zeros(Int64, (3^ndim*ne)^ndim)
+            V = zeros(Float64, (3^ndim*ne)^ndim)  
+        end
         # element loop
         if ndim == 2
             # gaussian quadrature points for the element [-1,1]x[-1,1] 
@@ -80,7 +122,7 @@ module fem
             
             x = [ξ[1], ξ[2], ξ[2], ξ[1]]
             y = [η[1], η[1], η[2], η[2]]
-    
+
         elseif ndim == 3
             # gaussian quadrature points for the element [-1,1]x[-1,1]x[-1,1] 
             ξ, w_ξ = gaussian_quadrature(-1,1)
@@ -93,22 +135,16 @@ module fem
             y = [η[1], η[1], η[2], η[2], η[1], η[1], η[2], η[2]]
             z = [ζ[1], ζ[1], ζ[1], ζ[1], ζ[2], ζ[2], ζ[2], ζ[2]]
         end
-    
+
         for e in 1:ne^ndim
-    
             coords = NodeList[:,IEN[e,:]] # get the coordinates of the nodes of the element
             
             # integration loop
             for gp in 1:2^ndim
+                N, ΔN = basis_function(x[gp],y[gp], nothing, FunctionClass) 
 
-                if ndim == 2
-                    N, ΔN = basis_function(x[gp], y[gp])
-                elseif ndim == 3    
-                    N, ΔN = basis_function(x[gp], y[gp], z[gp])
-                end
-    
                 Jac  = coords*ΔN # Jacobian matrix [dx/dxi dx/deta; dy/dxi dy/deta]
-    
+
                 w = wpoints[gp]*abs(det(Jac))
                 invJ = inv(Jac)
                 dNdX = ΔN*invJ
@@ -117,7 +153,7 @@ module fem
                 # loop between basis functions of the element
                 for i in 1:szN
                     for j in 1:szN
-                        inz = (szN)^2*(e-1) + szN*(i-1) + j # index for the COO sparse matrix
+                        inz = (szN)^ndim*(e-1) + szN*(i-1) + j # index for the COO sparse matrix
                         E[inz] = IEN[e,i] # row index 
                         J[inz] = IEN[e,j] # column index
                         V[inz] += w*dot(dNdX[i,:],dNdX[j,:])# inner product of the gradient of the basis functions
@@ -125,9 +161,9 @@ module fem
                 end
             end
         end
-    
+
         K = sparse(E,J,V)
-    
+
         return K
     end
 
