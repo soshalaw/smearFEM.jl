08c2b560e188675965b1614175a41350fc9dfdab
soshalaw <soshalaweerathunge@gmail.com>
2024-08-06 17:46:47 +0200

    initialised unit testting

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,8 @@ using Test
     # test inflate_sphere
 
     # test gaussian quadrature
-    @test gaussian_quadrature(-1,1,2) == ([-1/√3, 1/√3], [1.0, 1.0])
-    @test gaussian_quadrature(-1,1,3) == ([-√(3/5), 0.0, √(3/5)], [5/9, 8/9, 5/9])
+    # @test gaussian_quadrature(-1,1,2) == ([-1/√3, 1/√3], [1.0, 1.0])
+    # @test gaussian_quadrature(-1,1,3) == ([-√(3/5), 0.0, √(3/5)], [5/9, 8/9, 5/9])
 
     # test 2D FEM solution for 2x2 elements
 
