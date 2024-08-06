33b24eeaf1e4244f643688d66b702ea6476954a6
soshalaw <soshalaweerathunge@gmail.com>
2024-08-06 10:26:48 +0200

    added CI files

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using smearFEM
+using Documenter
+
+DocMeta.setdocmeta!(smearFEM, :DocTestSetup, :(using smearFEM); recursive=true)
+
+makedocs(;
+    modules=[smearFEM],
+    authors="soshalaw <soshalaweerathunge@gmail.com> and contributors",
+    sitename="smearFEM.jl",
+    format=Documenter.HTML(;
+        canonical="https://soshalaw.github.io/smearFEM.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/soshalaw/smearFEM.jl",
+    devbranch="main",
+)
