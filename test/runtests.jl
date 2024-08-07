d02fd29611c22660fe58fb48f2df9ca3315acd9c
soshalaw <soshalaweerathunge@gmail.com>
2024-08-07 17:22:54 +0200

    added basis_functions and gaussian_quadrature

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,28 @@ using Test
 @testset "smearFEM.jl" begin
     # test mesh generation
 
-    # test inflate_sphere
+    # test basis functions
+    @test basis_function(-1)[1] == [1.0, 0.0]
+    @test basis_function(1)[1] == [0.0, 1.0]
+
+    @test basis_function(-1,-1)[1] == [1.0, 0.0, 0.0, 0.0]
+    @test basis_function(1,-1)[1] == [0.0, 1.0, 0.0, 0.0]
+    @test basis_function(1,1)[1] == [0.0, 0.0, 1.0, 0.0]
+    @test basis_function(-1,1)[1] == [0.0, 0.0, 0.0, 1.0]
+
+    @test basis_function(-1,-1,-1)[1] == [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    @test basis_function(1,-1,-1)[1] == [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    @test basis_function(1,1,-1)[1] == [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    @test basis_function(-1,1,-1)[1] == [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]
+    @test basis_function(-1,-1,1)[1] == [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+    @test basis_function(1,-1,1)[1] == [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]
+    @test basis_function(1,1,1)[1] == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]
+    @test basis_function(-1,1,1)[1] == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+
 
     # test gaussian quadrature
-    # @test gaussian_quadrature(-1,1,2) == ([-1/√3, 1/√3], [1.0, 1.0])
-    # @test gaussian_quadrature(-1,1,3) == ([-√(3/5), 0.0, √(3/5)], [5/9, 8/9, 5/9])
+    @test gaussian_quadrature(-1,1,2) == ([-1/√3, 1/√3], [1.0, 1.0])
+    @test gaussian_quadrature(-1,1,3) == ([-√(3/5), 0.0, √(3/5)], [5/9, 8/9, 5/9])
 
     # test 2D FEM solution for 2x2 elements
 
