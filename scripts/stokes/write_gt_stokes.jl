67f56d76d6c6c76e0ba312b04e0f12b1c3696bf1
soshala <soshalaweerathunge@gmail.com>
2025-09-07 02:58:18 +0200

    gt data

--- scripts/stokes/write_gt_stokes.jl
+++ scripts/stokes/write_gt_stokes.jl
@@ -22,21 +22,15 @@ function const_vel(r::Float64, h::Float64, ndim::Int, FunctionClass_u::String, n
   F_ext::Float64 = 1500.0
   F_::Vector{Float64} = -F_ext*ones(Float64, round(Int, (sim_time/t_steps))) # force applied to the cylinder in N
 
-  # β_list = [100.0]
-  # η_list = [50.0]
   i::Int = 1
 
   for gt_β::Float64 in β_list
-    if gt_β < 100
-      F = F_*100
-    else
-      F = F_*gt_β
-    end
+    F = F_*3*gt_β
     for gt_η::Float64 in η_list
 
       model, scene = def_problem(r, h, ne, gt_η, ndim, FunctionClass_u, nDof_u, FunctionClass_p, nDof_p, "Q2", gt_β, F, control, viscosity_type, 
                     sim_time, t_steps)
-      filepath = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/sim_experiments/ground_truth/fem_runs/Stokes/$control/$viscosity_type/$i")
+      filepath = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/sim_experiments/ground_truth/fem/Stokes/$control/$viscosity_type/$i")
       write_sim_data(model, scene, camera_matrix, camera_pose, filepath)
 
       i += 1
@@ -51,27 +45,23 @@ function bulk_vel(r::Float64, h::Float64, ndim::Int, FunctionClass_u::String, nD
   viscosity_type::String = "bulk_viscosity" # "constant" or "bulk_viscosity"
 
   # simulation parameters for the ground truth
-  sim_time::Float64 = 50.0
-  steps::Float64 = 500.0
+  sim_time::Float64 = 40.0
+  steps::Float64 = 400.0
   t_steps::Float64 = sim_time/steps
 
-  F_ext::Float64 = 200.0 # force applied to the cylinder in N
+  F_ext::Float64 = 400.0 # force applied to the cylinder in N
   F_::Vector{Float64} = -F_ext*ones(Float64, round(Int, (sim_time/t_steps))) # force applied to the cylinder in N
 
   i::Int = 1
 
   for gt_β::Float64 in β_list
-    if gt_β <= 100
-      F = F_*100
-    else
-      F = F_*gt_β
-    end
+    F = F_*3*gt_β
 
     for gt_η::Float64 in η_list
 
       model, scene = def_problem(r, h, ne, gt_η, ndim, FunctionClass_u, nDof_u, FunctionClass_p, nDof_p, "Q2", gt_β, F, control, viscosity_type, 
               sim_time, t_steps)
-      filepath = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/sim_experiments/ground_truth/FEM/Stokes/$control/$viscosity_type/$i")
+      filepath = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/sim_experiments/ground_truth/fem/Stokes/$control/$viscosity_type/$i")
       write_sim_data(model, scene, camera_matrix, camera_pose, filepath)
 
       set_file(string(filepath,"/Results/plots"))
@@ -98,7 +88,7 @@ function main()
   FunctionClass_p::String = "Q1"
   nDof_p::Int = 1  # number of degree of freedom per node
 
-  ne::Int = 2 # number of elements in the mesh for the ground truth
+  ne::Int = 15 # number of elements in the mesh for the ground truth
 
   camera_matrix = [[8*2048/7.07, 0.0, 2048/2] [0.0, 8*1536/5.3, 1536/2] [0.0, 0.0, 1.0]]'
   camera_pose = scale*[0 -0.5 2.75]'   # camera position in mm
