71ef79ba14eedf9622cc68f6358ceb543d186b55
Soshala Weerathunge <soshalaweerathunge@gmail.com>
2025-09-09 22:40:05 +0200

    Update test_opt_stokes.jl

--- scripts/model_optimization/test_opt_stokes.jl
+++ scripts/model_optimization/test_opt_stokes.jl
@@ -610,7 +610,7 @@ function main()
     # refine_list = [1, 2, 3] # refinement levels, ne = ne_exp^refine
     refine_list = [2] # refinement levels, ne = ne_exp^refine
     control = "force" # "force" or "velocity"
-    viscosity_type = "bulk_viscosity"
+    viscosity_type_list = ["bulk_viscosity", "constant"]
     FunctionClass_x_gt_list = ["Q2"] # Function space for the ground truth
 
     exp_size = size(FunctionClass_x_List,1)*size(refine_list,1)
@@ -618,30 +618,32 @@ function main()
     β_mat = zeros(30, exp_size)
 
     WRITE_GT = true
-    for FunctionClass_x_gt in FunctionClass_x_gt_list
-        run_id = 1
-        for β_gt in β_gt_list
-            for η_gt in η_gt_list
-
-                filepath = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/sim_experiments/cost_function_test/optimization/Stokes/$control/model_validation/$viscosity_type/$ne_gt/$run_id")
-                filepath_gt = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/sim_experiments/ground_truth/fem/Stokes/$control/$viscosity_type/$ne_gt/$run_id")
-                
-                for ref in refine_list
-                    ne = ne_exp^ref
-                    @info "Running optimization with ne = $ne"
-                    for FunctionClass_x in FunctionClass_x_List
-                        @info "Running optimization with FunctionClass_x = $FunctionClass_x with $ne elements"
-
-                        exp_params = Dict("FunctionClass_x" => FunctionClass_x, "FunctionClass_u" => "Q2", "FunctionClass_p" => "Q1", "ne_gt" => ne_gt, "ne_exp" => ne, 
-                                    "β_gt" => β_gt, "η_gt" => η_gt, "WRITE_GT" => WRITE_GT, "filepath" => filepath, "filepath_gt"=>filepath_gt, "control" => control, 
-                                    "viscosity_type"=>viscosity_type, "FunctionClass_x_gt" => FunctionClass_x_gt)
-
-                        test_opt_const(exp_params)
-                        WRITE_GT = true
+    for viscosity_type in viscosity_type_list
+        for FunctionClass_x_gt in FunctionClass_x_gt_list
+            run_id = 1
+            for β_gt in β_gt_list
+                for η_gt in η_gt_list
+    
+                    filepath = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/sim_experiments/cost_function_test/optimization/Stokes/$control/model_validation/$viscosity_type/$ne_gt/$run_id")
+                    filepath_gt = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/sim_experiments/ground_truth/fem/Stokes/$control/$viscosity_type/$ne_gt/$run_id")
+                    
+                    for ref in refine_list
+                        ne = ne_exp^ref
+                        @info "Running optimization with ne = $ne"
+                        for FunctionClass_x in FunctionClass_x_List
+                            @info "Running optimization with FunctionClass_x = $FunctionClass_x with $ne elements"
+    
+                            exp_params = Dict("FunctionClass_x" => FunctionClass_x, "FunctionClass_u" => "Q2", "FunctionClass_p" => "Q1", "ne_gt" => ne_gt, "ne_exp" => ne, 
+                                        "β_gt" => β_gt, "η_gt" => η_gt, "WRITE_GT" => WRITE_GT, "filepath" => filepath, "filepath_gt"=>filepath_gt, "control" => control, 
+                                        "viscosity_type"=>viscosity_type, "FunctionClass_x_gt" => FunctionClass_x_gt)
+    
+                            test_opt_const(exp_params)
+                            WRITE_GT = true
+                        end
                     end
+                    # post_analysis(filepath_gt, filepath)
+                    run_id = run_id + 1
                 end
-                post_analysis(filepath_gt, filepath)
-                run_id = run_id + 1
             end
         end
     end
