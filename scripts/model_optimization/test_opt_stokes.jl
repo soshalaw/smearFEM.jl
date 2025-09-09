71932dfacf240d7f9c39059d0ddfc6896367c619
Soshala Weerathunge <soshalaweerathunge@gmail.com>
2025-09-10 00:38:21 +0200

    Update test_opt_stokes.jl

--- scripts/model_optimization/test_opt_stokes.jl
+++ scripts/model_optimization/test_opt_stokes.jl
@@ -612,7 +612,7 @@ function main()
     # refine_list = [1, 2, 3] # refinement levels, ne = ne_exp^refine
     refine_list = [2] # refinement levels, ne = ne_exp^refine
     control = "force" # "force" or "velocity"
-    viscosity_type_list = ["constant"]
+    viscosity_type_list = ["bulk_viscosity"]
     FunctionClass_x_gt_list = ["Q2"] # Function space for the ground truth
 
     exp_size = size(FunctionClass_x_List,1)*size(refine_list,1)
