4a8d53e9526ce2555ce005a84304e8f1c6bcf996
Soshala Weerathunge <soshalaweerathunge@gmail.com>
2025-09-12 16:16:38 +0200

    changed plotting lw

--- src/visualization/plotting.jl
+++ src/visualization/plotting.jl
@@ -71,17 +71,17 @@ function plot_mesh(NodeList, IEN)
         for i in iter
             x = NodeList[1,IEN[i,:]]
             y = NodeList[2,IEN[i,:]]
-            Plots.plot!(x, y, marker=1.5, lw=0.5, label="")
+            Plots.plot!(x, y, marker=1.5, lw=0.5, label="", lw=3)
         end
 
     elseif sz == 3
-        Plots.scatter3d(NodeList[1,:], NodeList[2,:], NodeList[3,:], markersize=2, label="", dpi=:400)
+        Plots.scatter3d(NodeList[1,:], NodeList[2,:], NodeList[3,:], markersize=2, label="", dpi=:400, lw=3)
         iter = 1:size(IEN,1)
         for i in iter
             x = NodeList[1,IEN[i,:]]
             y = NodeList[2,IEN[i,:]]
             z = NodeList[3,IEN[i,:]]
-            Plots.plot3d!(x, y, z,marker=1.5, lw=0.5, label="", dpi=:400)
+            Plots.plot3d!(x, y, z,marker=1.5, lw=0.5, label="", dpi=:400, lw=3)
         end
 
         Plots.xlabel!(L"x")
@@ -164,10 +164,10 @@ function animate2D(;BorderNodes2D=nothing, fields2D=nothing, p=nothing, q=nothin
             plt = set_plot(22)
 
             if !isnothing(p)   
-                Plots.plot!(p[i],q[i], legend=true, labels="Simulation", aspect_ratio = :equal, dpi=:400) #, marker = :circle, markersize = 2)
+                Plots.plot!(p[i],q[i], legend=true, labels="Simulation", aspect_ratio = :equal, dpi=:400, lw=3) #, marker = :circle, markersize = 2)
             end
             if !isnothing(pObs)
-                Plots.plot!(pObs[i],qObs[i], labels="Observation", aspect_ratio = :equal, dpi=:400)
+                Plots.plot!(pObs[i],qObs[i], labels="Observation", aspect_ratio = :equal, dpi=:400, lw=2)
             end
             if !isnothing(fields2D)
                 Plots.scatter!(fields2D[i][1,:], fields2D[i][2,:], ms=:4, mc=:royalblue, ma=:0.7, legend=true, labels="Surface Nodes", aspect_ratio = :equal, 
@@ -216,10 +216,10 @@ function animate_2D_comp(;borders=nothing, filepath="images/2D_grid.gif")
             plt = set_plot(22)
 
             if !isnothing(p)   
-                Plots.plot!(p[i],q[i], legend=true, labels="Simulation", aspect_ratio = :equal, dpi=:400)
+                Plots.plot!(p[i],q[i], legend=true, labels="Simulation", aspect_ratio = :equal, dpi=:400, lw=2)
             end
             if !isnothing(pObs)
-                Plots.plot!(pObs[i],qObs[i], labels="Observation", aspect_ratio = :equal, dpi=:400)
+                Plots.plot!(pObs[i],qObs[i], labels="Observation", aspect_ratio = :equal, dpi=:400, lw=2)
             end
             if !isnothing(fields2D)
                 Plots.scatter!(fields2D[i][1,:], fields2D[i][2,:], ms=:4, mc=:royalblue, ma=:0.7, legend=true, labels="Surface Nodes", aspect_ratio = :equal, 
@@ -340,8 +340,8 @@ function plot_matches(simborderfields, p, q, pObs, qObs, pairsList, filepath::St
         borderp = simborderfields[i][1,:]
         borderq = simborderfields[i][2,:]
         plt = Plots.plot(1,xlims=(0,2048), ylims=(0,1536), xlabel="x",ylabel="y",title="Prospective Projection of the 3D Grid", label="", dpi=400)
-        Plots.plot!(p[i],q[i], legend=true, labels="Simulation",  dpi=:400)
-        Plots.plot!(pObs[i],qObs[i], labels="Observation",  dpi=:400)
+        Plots.plot!(p[i],q[i], legend=true, labels="Simulation",  dpi=:400, lw=2)
+        Plots.plot!(pObs[i],qObs[i], labels="Observation",  dpi=:400, lw=2)
         pairs = pairsList[i]
         pairtIter = 1:size(pairs,1)
         for j in pairtIter
