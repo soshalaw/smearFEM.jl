b0e038e4cb9b2cebf94e206b6b2a29b1f1cec378
soshala <soshalaweerathunge@gmail.com>
2025-11-18 16:15:50 +0100

    simulations of synthetic data

--- scripts/model_optimization/test_opt_stokes.jl
+++ scripts/model_optimization/test_opt_stokes.jl
@@ -1728,7 +1728,7 @@ function optimize_syn()
 
     FunctionClass_x_List = ["Q2"]
     # refine_list = [1, 2, 3] # refinement levels, ne = ne_exp^refine
-    refine_list = [6] #, 12, 16] # refinement levels, ne = ne_exp^refine
+    refine_list = [2, 4, 6, 8] # refinement levels, ne = ne_exp^refine
     noise_level_list = [0.0] # 0.5 1.0]
     control = "force" # "force" or "velocity"
     viscosity_type_list = ["constant"]
