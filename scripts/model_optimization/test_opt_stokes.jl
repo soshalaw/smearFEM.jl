5396735b4f2d30e964df7c785b97cd4054816ffe
soshala <soshalaweerathunge@gmail.com>
2025-11-21 17:24:23 +0100

    experiments

--- scripts/model_optimization/test_opt_stokes.jl
+++ scripts/model_optimization/test_opt_stokes.jl
@@ -2027,7 +2027,7 @@ function optimize_syn()
                         for sim_time_exp::Float16 in sim_time_exp_list
                             filepath_res = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/experiments/syn_data/optimization/Stokes/$control/$viscosity_type/Q2_16/$dir/Q2_$ne/simtime_$(sim_time_exp)")
                             # ne = ne_exp^ref
-                            @info "Running optimization with ne = $ne and simulation time = $sim_time_exp"
+                            @info "Running optimization with ne = $ne and simulation time = $sim_time_exp with noise level = $noise_level"
                             for FunctionClass_x in FunctionClass_x_List
                                 @info "Running optimization with FunctionClass_x = $FunctionClass_x with $ne elements"
 
