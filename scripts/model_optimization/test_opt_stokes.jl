7b95ac1619d92bb26a1f1b8d5cb42f889014d0f9
soshala <soshalaweerathunge@gmail.com>
2025-12-11 12:44:30 +0100

    noise analysis 1.0

--- scripts/model_optimization/test_opt_stokes.jl
+++ scripts/model_optimization/test_opt_stokes.jl
@@ -2604,7 +2604,7 @@ function optimize_sim()
             filepath_gt = string(_filepath_gt,"/",dir)
             for ne in refine_list
                 if ne == 6 && viscosity_type == "constant"
-                    noise_level_list = [0.5]
+                    noise_level_list = [1.0]
                 else
                     noise_level_list = [0.0]
                 end
