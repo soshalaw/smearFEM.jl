e688af359d11f092184bb23000930e29de730462
soshala <soshalaweerathunge@gmail.com>
2026-01-05 00:09:58 +0100

    real exp bulk visc

--- scripts/model_optimization/test_opt_stokes.jl
+++ scripts/model_optimization/test_opt_stokes.jl
@@ -3788,7 +3788,7 @@ function optimize_real()
                 println("Simulation time: $sim_time_exp seconds")
                 for ne in refine_list
                     for FunctionClass_x in FunctionClass_x_List
-                        filepath_res = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/experiments/physical_data/optimization/$dir/$(FunctionClass_x)_$(ne)/simtime_$(sim_time_exp)/noise_$(noise_level)/$window")
+                        filepath_res = string("/home/soshala/SMEAR-PhD/SMEAR-DataFiles/Data/experiments/physical_data/optimization/$dir/$(FunctionClass_x)_$(ne)/simtime_$(sim_time_exp)/$window")
                         @info "Running optimization with FunctionClass_x = $FunctionClass_x with $ne elements"
                         
                         exp_params = Dict("FunctionClass_x" => FunctionClass_x, "FunctionClass_u" => "Q2", "FunctionClass_p" => "Q1", "ne_exp" => ne, "sim_time_exp" => sim_time_exp, 
