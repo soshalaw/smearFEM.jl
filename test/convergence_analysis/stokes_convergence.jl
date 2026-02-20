1efc2420fc9150ce78514fc05caf3d7e6c3d4e90
soshala <soshalaweerathunge@gmail.com>
2026-02-20 14:39:12 +0100

    [CHANGE] changed the convergence test to cosider multiple steps

--- test/convergence_analysis/stokes_convergence.jl
+++ test/convergence_analysis/stokes_convergence.jl
@@ -264,7 +264,7 @@ function mesh_convergence_analysis()
     est_μ_list, gradList, borderPts2DList, fields, pos3D, pos2D, splinep, splineq = simulate(model_ref, scene_ref, conditions)   
 
     mesh_sz_ = [2, 4, 6, 8, 10, 12, 14, 16]
-    mesh_sz = mesh_sz_
+    mesh_sz = reverse(mesh_sz_)
     iter_index = 1
     h_ref = 0.0
     border_ref = borderPts2DList
