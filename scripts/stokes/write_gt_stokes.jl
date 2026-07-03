da41fd6dac06aec5f2a4b035f14ec18d48b9edb7
soshala <soshalaweerathunge@gmail.com>
2026-07-03 18:28:50 +0200

    [CHANGE] corrected error in animate_fields

--- scripts/stokes/write_gt_stokes.jl
+++ scripts/stokes/write_gt_stokes.jl
@@ -174,7 +174,7 @@ function main(; use_parallel::Bool=true, calibrate::Bool=false, max_workers::Int
     h::Float64 = 40.0  # height of the cylinder in mm
     ne_gt::Float64 = 16 # number of elements in the mesh for the ground truth
     # ne_gt = 10.0
-    β_gt_list = [0.01, 10, 50, 100 ,500, 1e3] # penalty parameters for the ground truth [2e3, 5e3, 1e4, 1e5, 1e10]
+    β_gt_list = [100] # penalty parameters for the ground truth [2e3, 5e3, 1e4, 1e5, 1e10]
     η_gt_list = [1e2] # viscosity values for the ground truth in kg/(mm⋅s)
     z_angle_list = [0.0, 30.0, 60.0] # rotation angles around the z-axis in degrees 
 
@@ -207,7 +207,7 @@ function main(; use_parallel::Bool=true, calibrate::Bool=false, max_workers::Int
             for η_gt in η_gt_list
                 F_ext = _get_F_ext(β_gt)
                 # filepath_gt = resolve_data_path("ground_truth/sim_data/Stokes/$control/$viscosity_type/$(mesh_label)_$(ne_gt)/$geometry/$run_id")
-                filepath_gt = resolve_data_path("ground_truth/sim_data/Stokes/force/constant/Hex_2/convergence_analysis/experiment_mesh_convergence_analysis")
+                filepath_gt = resolve_data_path("ground_truth/sim_data/Stokes/force/constant/Hex_2/convergence_analysis/experiment_mesh_convergence_analysis/")
                 
                 exp_params = Dict(
                     "element_shape_u" => element_shape_u,
