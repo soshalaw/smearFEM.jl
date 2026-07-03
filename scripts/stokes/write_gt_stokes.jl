dec429fa4d02a676e0655ad09547c5bccfacd463
soshala <soshalaweerathunge@gmail.com>
2026-07-03 15:29:51 +0200

    [High res mesh generation for convergence exp]

--- scripts/stokes/write_gt_stokes.jl
+++ scripts/stokes/write_gt_stokes.jl
@@ -173,7 +173,7 @@ end
 
 function main(; use_parallel::Bool=true, calibrate::Bool=false, max_workers::Int=-1, memory_per_experiment_mb::Float64=512.0)
     # parameters for the optimization
-    r::Float64 = 25.0*2  # radius of the cylinder in mm
+    r::Float64 = 25.0  # radius of the cylinder in mm
     h::Float64 = 40.0  # height of the cylinder in mm
     ne_gt::Float64 = 16 # number of elements in the mesh for the ground truth
     # ne_gt = 10.0
@@ -193,7 +193,7 @@ function main(; use_parallel::Bool=true, calibrate::Bool=false, max_workers::Int
     basis_order_x::Int      = 2
     mesh_label = "$(element_shape_u)$(basis_order_u)"
 
-    geometry::Symbol = :cube # :cylinder or :cube
+    geometry::Symbol = :cylinder # :cylinder or :cube
     edge_radius::Float64 = 3.0   # fillet radius on vertical edges (mm)
 
     sim_time_gt::Float64 = 30.0 # simulation time in seconds
@@ -209,8 +209,8 @@ function main(; use_parallel::Bool=true, calibrate::Bool=false, max_workers::Int
         for β_gt in β_gt_list
             for η_gt in η_gt_list
                 F_ext = _get_F_ext(β_gt)
-                filepath_gt = resolve_data_path("ground_truth/sim_data/Stokes/$control/$viscosity_type/$(mesh_label)_$(ne_gt)/$geometry/$run_id")
-                # filepath_gt = resolve_data_path("ground_truth/sim_data/Stokes/force/constant/Hex_2/convergence_analysis/experiment_mesh_convergence_analysis")
+                # filepath_gt = resolve_data_path("ground_truth/sim_data/Stokes/$control/$viscosity_type/$(mesh_label)_$(ne_gt)/$geometry/$run_id")
+                filepath_gt = resolve_data_path("ground_truth/sim_data/Stokes/force/constant/Hex_2/convergence_analysis/experiment_mesh_convergence_analysis")
                 
                 exp_params = Dict(
                     "element_shape_u" => element_shape_u,
@@ -301,4 +301,4 @@ end
 
 # Usage: main(use_parallel=true, calibrate=false) or main(use_parallel=true, calibrate=true)
 # Default: Run with current F_ext values (to calibrate: pass calibrate=true)
-main(use_parallel=true, calibrate=false, memory_per_experiment_mb=1024.0)
+main(use_parallel=false, calibrate=false, memory_per_experiment_mb=1024.0)
