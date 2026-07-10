5247c2e63cd0e11e1b75639e6ea843a1a5a1cef0
soshala <soshalaweerathunge@gmail.com>
2026-07-10 23:16:41 +0200

    [CHANGE] inv conv analysis

--- scripts/model_optimization/test_opt_stokes.jl
+++ scripts/model_optimization/test_opt_stokes.jl
@@ -368,7 +368,10 @@ function optimize(exp_params::Dict)
     end
     
     for (i,z_angle) in enumerate(z_angles)
-        
+        if i != 1
+            @info "Processing view $i with z_angle = $z_angle degrees"
+            continue
+        end
         printstyled("Processing view $i with z_angle = $z_angle degrees\n"; color = :blue)
         if data_type == "physical"  || viscosity_model == "carreau"
             η_start = exp_params["η_start"]
